40343f6e55440871a3e29e383c62356d9a696b85
Nguyễn Ngọc Tân <uranashel44@gmail.com>
2025-06-12 14:14:48 +0700

    Add clearing cache for large image data

--- src/index.tsx
+++ src/index.tsx
@@ -211,6 +211,8 @@ interface FastImageStaticProperties {
     priority: typeof priority
     cacheControl: typeof cacheControl
     preload: (sources: Source[]) => void
+    clearMemoryCache: () => Promise<void>
+    clearDiskCache: () => Promise<void>
 }
 
 const FastImage: React.ComponentType<FastImageProps> &
@@ -225,6 +227,9 @@ FastImage.priority = priority
 FastImage.preload = (sources: Source[]) =>
     FastImageViewNativeModule.preload(sources)
 
+FastImage.clearMemoryCache = () => FastImageViewNativeModule?.clearMemoryCache?.()
+
+FastImage.clearDiskCache = () => FastImageViewNativeModule?.clearDiskCache?.()
 const styles = StyleSheet.create({
     imageContainer: {
         overflow: 'hidden',
